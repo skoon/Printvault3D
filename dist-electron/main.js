44dbfdd832dbff97b99d7f31b7e5e42b7fab966c
Scott Koon <scottckoon@gmail.com>
2026-05-28 18:30:21 -0700

    Changed electron build to handle web or local file storage.

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,108 +1,146 @@
-import { app as n, BrowserWindow as v, ipcMain as a, dialog as _ } from "electron";
-import r from "path";
-import { fileURLToPath as A } from "url";
-import h from "fs/promises";
-const D = {
+import { app, BrowserWindow, ipcMain, dialog } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+const DEFAULT_STATE = {
   models: [],
   rootPath: null,
   apiKey: ""
 };
-class b {
+class Storage {
   constructor() {
-    this.statePath = r.join(n.getPath("userData"), "state.json"), this.state = { ...D };
+    this.statePath = path.join(app.getPath("userData"), "state.json");
+    this.state = { ...DEFAULT_STATE };
   }
   async init() {
     try {
-      const t = await h.readFile(this.statePath, "utf-8");
-      this.state = JSON.parse(t);
+      const data = await fs.readFile(this.statePath, "utf-8");
+      this.state = JSON.parse(data);
     } catch {
       await this.save();
     }
   }
   async save() {
-    await h.writeFile(this.statePath, JSON.stringify(this.state, null, 2), "utf-8");
+    await fs.writeFile(this.statePath, JSON.stringify(this.state, null, 2), "utf-8");
   }
-  async saveModels(t) {
-    this.state.models = t, await this.save();
+  async saveModels(models) {
+    this.state.models = models;
+    await this.save();
   }
   async loadModels() {
     return this.state.models;
   }
-  async saveRootPath(t) {
-    this.state.rootPath = t, await this.save();
+  async saveRootPath(dirPath) {
+    this.state.rootPath = dirPath;
+    await this.save();
   }
   async getRootPath() {
     return this.state.rootPath;
   }
-  async saveApiKey(t) {
-    this.state.apiKey = t, await this.save();
+  async saveApiKey(key) {
+    this.state.apiKey = key;
+    await this.save();
   }
   async getApiKey() {
     return this.state.apiKey;
   }
 }
-const i = new b(), j = A(import.meta.url), F = r.dirname(j), K = !n.isPackaged;
-let l = null;
-function g() {
-  l = new v({
+const storage = new Storage();
+const __filename$1 = fileURLToPath(import.meta.url);
+const __dirname$1 = path.dirname(__filename$1);
+const isDev = !app.isPackaged;
+let mainWindow = null;
+function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 1280,
     height: 900,
     minWidth: 800,
     minHeight: 600,
     webPreferences: {
-      preload: r.join(F, "preload.js"),
-      contextIsolation: !0,
-      nodeIntegration: !1
+      preload: path.join(__dirname$1, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false
     }
-  }), K ? (l.loadURL("http://localhost:3000"), l.webContents.openDevTools()) : l.loadFile(r.join(n.getAppPath(), "dist", "index.html"));
+  });
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist", "index.html"));
+  }
 }
-n.whenReady().then(() => {
-  i.init(), g(), n.on("activate", () => {
-    v.getAllWindows().length === 0 && g();
+app.whenReady().then(() => {
+  storage.init();
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
 });
-n.on("window-all-closed", () => {
-  process.platform !== "darwin" && n.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
-a.handle("pick-directory", async () => {
-  const e = await _.showOpenDialog(l, {
+ipcMain.handle("pick-directory", async () => {
+  const result = await dialog.showOpenDialog(mainWindow, {
     properties: ["openDirectory"]
   });
-  return e.canceled || e.filePaths.length === 0 ? null : e.filePaths[0];
+  if (result.canceled || result.filePaths.length === 0) {
+    return null;
+  }
+  return result.filePaths[0];
 });
-a.handle("scan-directory", async (e, t) => {
-  const o = [".stl", ".obj", ".3mf"], c = [];
-  async function d(y, f = "", p = []) {
-    const P = await h.readdir(y, { withFileTypes: !0 });
-    for (const s of P) {
-      const m = r.join(y, s.name);
-      if (s.isFile()) {
-        const u = r.extname(s.name).toLowerCase();
-        if (o.includes(u)) {
-          const w = await h.stat(m);
-          c.push({
+ipcMain.handle("scan-directory", async (_event, dirPath) => {
+  const ALLOWED_EXTENSIONS = [".stl", ".obj", ".3mf"];
+  const models = [];
+  async function walk(dir, currentPath = "", dirTags = []) {
+    const entries = await fs.readdir(dir, { withFileTypes: true });
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry.name);
+      if (entry.isFile()) {
+        const ext = path.extname(entry.name).toLowerCase();
+        if (ALLOWED_EXTENSIONS.includes(ext)) {
+          const stat = await fs.stat(fullPath);
+          models.push({
             id: crypto.randomUUID(),
-            name: s.name,
-            path: `${f}/${s.name}`,
-            extension: u,
-            size: w.size,
-            lastModified: w.mtimeMs,
+            name: entry.name,
+            path: `${currentPath}/${entry.name}`,
+            extension: ext,
+            size: stat.size,
+            lastModified: stat.mtimeMs,
             tags: [],
-            directoryTags: p
+            directoryTags: dirTags
           });
         }
-      } else s.isDirectory() && await d(m, `${f}/${s.name}`, [...p, s.name]);
+      } else if (entry.isDirectory()) {
+        await walk(fullPath, `${currentPath}/${entry.name}`, [...dirTags, entry.name]);
+      }
     }
   }
-  return await d(t), c;
+  await walk(dirPath);
+  return models;
 });
-a.handle("save-models", async (e, t) => i.saveModels(t));
-a.handle("load-models", async () => i.loadModels());
-a.handle("save-root-path", async (e, t) => i.saveRootPath(t));
-a.handle("get-root-path", async () => i.getRootPath());
-a.handle("save-api-key", async (e, t) => i.saveApiKey(t));
-a.handle("get-api-key", async () => i.getApiKey());
-a.handle("read-file", async (e, t) => {
-  const o = await h.readFile(t);
-  return o.buffer.slice(o.byteOffset, o.byteOffset + o.byteLength);
+ipcMain.handle("save-models", async (_event, models) => {
+  return storage.saveModels(models);
+});
+ipcMain.handle("load-models", async () => {
+  return storage.loadModels();
+});
+ipcMain.handle("save-root-path", async (_event, dirPath) => {
+  return storage.saveRootPath(dirPath);
+});
+ipcMain.handle("get-root-path", async () => {
+  return storage.getRootPath();
+});
+ipcMain.handle("save-api-key", async (_event, key) => {
+  return storage.saveApiKey(key);
+});
+ipcMain.handle("get-api-key", async () => {
+  return storage.getApiKey();
+});
+ipcMain.handle("read-file", async (_event, filePath) => {
+  const buffer = await fs.readFile(filePath);
+  return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
 });
